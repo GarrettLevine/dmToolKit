ddea311b794eb1b3e7cb3f48fa545cb6841a598a
Garrett Levine <garrlevine@gmail.com>
2016-06-06 23:37:28 -0400

    Added semantic ui. started web app

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 export class App extends Component {
   render() {
     return (
-      <h1>React simple</h1>
+    	<div className="ui visible left demo vertical inverted sidebar labeled icon menu">
+	 				<a className="item">
+	 				  <i className="book icon"></i>
+		 				  Story
+	 				</a>
+	 				<a className="item">
+	 				  <i className="child layout icon"></i>
+		 				  NPCs
+	 				</a>
+	 				<a className="item">
+	 				  <i className="fa fa-shield icon"></i>
+		 				  Encounters
+	 				</a>
+    	</div>
     );
   }
 }
