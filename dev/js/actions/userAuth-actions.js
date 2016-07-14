0b75eb0e72c3420db0ef89114540dfe660ece20c
Garrett Levine <garrlevine@gmail.com>
2016-07-14 09:03:32 -0400

    User create actions/reducers

--- dev/js/actions/userAuth-actions.js
+++ dev/js/actions/userAuth-actions.js
@@ -1,5 +1,6 @@
 import * as types from './types';
 
+//update login email
 export function updateLoginEmail(email) {
   return {
     type: types.UPDATE_LOGIN_EMAIL,
@@ -7,9 +8,35 @@ export function updateLoginEmail(email) {
   };
 }
 
+//update login password
 export function updateLoginPassword(password) {
   return {
     type: types.UPDATE_LOGIN_PASSWORD,
     payload: password,
   };
 }
+
+//update user email create
+export function updateCreateEmail(email) {
+  return {
+    type: types.UPDATE_LOGIN_EMAIL,
+    payload: email,
+  };
+}
+
+//update user password create
+export function updateCreatePassword(email) {
+  return {
+    type: types.UPDATE_LOGIN_EMAIL,
+    payload: email,
+  };
+}
+
+//update user password confirm
+export function updateConfirmPasswrod(email) {
+  return {
+    type: types.UPDATE_LOGIN_EMAIL,
+    payload: email,
+  };
+}
+
--- dev/js/reducers/userAuth-reducer.js
+++ dev/js/reducers/userAuth-reducer.js
@@ -1,45 +1,75 @@
 //*************************************************
 //    C O M P O N E N T   I M P O R T S
 //*************************************************
-	import * as types from '../actions/types';
+  import * as types from '../actions/types';
 
 //*************************************************
 //    R E D U C E R S
 //*************************************************
 const initialState = {
-	login: {
-		email: '',
-		password: '',
-	},
-	signUp: {
-		email: '',
-		password: '',
-		confirmPass: '',
-	},
+  login: {
+    email: '',
+    password: '',
+  },
+  signUp: {
+    email: '',
+    password: '',
+    confirmPass: '',
+  },
 };
 
 export default function userAuth(state = initialState, action) {
-	switch (action.type) {
-		case types.UPDATE_LOGIN_EMAIL: {
-			return Object.assign({}, state, {
-			  login: { 
-			  	email: action.payload,
-			  	password: state.login.password,
-			  },
-			});
-		}
+  switch (action.type) {
+    case types.UPDATE_LOGIN_EMAIL: {
+      return Object.assign({}, state, {
+        login: { 
+          email: action.payload,
+          password: state.login.password,
+        },
+      });
+    }
 
-		case types.UPDATE_LOGIN_PASSWORD: {
-			return Object.assign({}, state, {
-			  login: {
-			  	email: state.login.email,
-			  	password: action.payload,
-			  },
-			});
-		}
+    case types.UPDATE_LOGIN_PASSWORD: {
+      return Object.assign({}, state, {
+        login: {
+          email: state.login.email,
+          password: action.payload,
+        },
+      });
+    }
 
-		default: {
-			return state;
-		}
-	};
+    case types.UPDATE_CREATE_EMAIL: {
+      return Object.assign({}, state, {
+        signUp: {
+          emai: action.payload,
+          password: state.signUp.password,
+          confirmPass: state.signUp.password,
+        },
+      });
+    }
+
+    case types.UPDATE_CREATE_PASSWORD: {
+      return Object.assign({}, state, {
+        signUp: {
+          emai: state.signUp.email,
+          password: action.payload,
+          confirmPass: state.signUp.password,
+        },
+      });
+    }
+
+    case types.UPDATE_CREATE_PASSWORD: {
+      return Object.assign({}, state, {
+        signUp: {
+          emai: state.signUp.email,
+          password: state.signUp.password,
+          confirmPass: action.payload,
+        },
+      });
+    }
+
+    default: {
+      return state;
+    }
+  };
 };
